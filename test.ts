05fa6793cdee202f0881e9bfe89a90af77476a94
tjk <tian_junk@163.com>
2019-07-23 10:10:01 +0800

    MCP23017

---
+++ test.ts
@@ -0,0 +1,5 @@
+// tests go here; this will not be compiled when this package is used as a library
+/* tests go here; this will not be compiled when this package is used as a library*/
+basic.forever(() => {
+    MCP23017.WritePin(0,85);
+})
